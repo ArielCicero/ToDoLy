32db4db58555a6663b1aa074ee58cad2a2a25a61
Ariel Cicero <ariel.cicero@gmail.com>
2019-03-02 15:57:33 +0100

    fixing edit-task-menu

--- controllers/menus/EditTaskMenuController.java
+++ controllers/menus/EditTaskMenuController.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import todoly.controllers.Controller;
 import todoly.interfaces.TaskListInterface;
+import todoly.util.enums.MenuOption;
 import todoly.views.menus.EditTaskMenuView;
 
 public class EditTaskMenuController extends Controller {
@@ -13,5 +14,14 @@ public class EditTaskMenuController extends Controller {
 							Integer.toString(taskList.getTasksAmount()),
 							Integer.toString(taskList.getTasksDoneAmount())
 					), scanner);
+	}
+	
+	@Override
+	protected void setMenuOption() {
+		validOptions.put("1", MenuOption.UPDATE_TASK_STATUS);
+		validOptions.put("2", MenuOption.UPDATE_TASK_TITLE);
+		validOptions.put("3", MenuOption.UPDATE_TASK_DUE_DATE);
+		validOptions.put("4", MenuOption.UPDATE_TASK_PROJECT);
+		validOptions.put("4", MenuOption.REMOVE_TASK);
 	}
 }
