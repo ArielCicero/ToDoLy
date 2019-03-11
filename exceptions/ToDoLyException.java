f1e78eb04c9b10613b56e73a8f664dcaaad9925f
Ariel Cicero <ariel.cicero@gmail.com>
2019-03-11 20:11:50 +0100

    deleting files

--- exceptions/ToDoLyException.java
+++
@@ -1,8 +0,0 @@
-package todoly.exceptions;
-
-@SuppressWarnings("serial")
-public class ToDoLyException extends RuntimeException {
-    public ToDoLyException(String message) {
-        super(message);
-    }
-}
--- interfaces/TaskListRepositoryInterface.java
+++
@@ -1,9 +0,0 @@
-package todoly.interfaces;
-
-public interface TaskListRepositoryInterface {
-    
-    TaskListInterface loadTaskList();
-    
-    void saveTaskList(TaskListInterface taskList);
-    
-}
